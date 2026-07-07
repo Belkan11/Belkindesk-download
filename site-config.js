e0a0e206e9250dc356b95f45d7e4e70a0183975d
Belkan <35707086+Belkan11@users.noreply.github.com>
2026-07-07 12:37:31 +0300

    rebuild site

---
+++ site-config.js
@@ -0,0 +1,27 @@
+/*
+ * Единственный файл, который нужно редактировать после обновления сборок.
+ * Вставляйте полные ссылки между кавычками и загружайте папку site на GitHub Pages.
+ */
+window.BELKIN_SITE_CONFIG = {
+  downloads: {
+    engineerWindows: "https://drive.google.com/file/d/1jKqIMStpLNtgpRbV0tDsUrCllPHJvoF2/view?usp=sharing",
+    medWindows: "https://drive.google.com/file/d/1hSgP9WUQCTxBDGvgR_Q29DvQfKcCErAG/view?usp=sharing",
+    medMac: "https://drive.google.com/file/d/1RaC6yCo9CyJKC_MpMbdeaU9TSnPDvIBB/view?usp=sharing",
+    zemlekopWindows: "https://drive.google.com/file/d/1r3ftpwnCzbTIjRwndX1XlWUIxA_MOzcx/view?usp=sharing"
+  },
+  builds: {
+    engineer: { version: "2.0", size: "38mb", requirements: "Windows 10/11 · x64" },
+    med: { version: "2.0", sizeWindows: "8mb", sizeMac: "уточняется", requirementsWindows: "Windows 10/11 · x64", requirementsMac: "macOS 12+" },
+    zemlekop: { version: "1.0", size: "38mb", requirements: "Windows 10/11 · x64" }
+  },
+  links: {
+    tokenClassic: "https://github.com/settings/tokens/new",
+    tokenSettings: "https://github.com/settings/tokens",
+    githubModels: "https://github.com/marketplace/models",
+    githubTokenDocs: "https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-personal-access-token-classic",
+    githubModelsDocs: "https://docs.github.com/en/github-models/quickstart",
+    manual: "https://drive.google.com/file/d/1ofaS4Oil_yx1CeYlv6GFqk9w3WjVWo6Q/view?usp=sharing",
+    githubToken: "https://drive.google.com/file/d/1GGAyZcxYSR3Hdpw84ENZZEuAW2bslCKw/view?usp=sharing",
+    files: "https://drive.google.com/drive/folders/1jdkI7i2nHZ-z1fzyLc-b77-gtFIJ-qxe?usp=sharing"
+  }
+};
